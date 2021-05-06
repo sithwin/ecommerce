eab8b96d80dc949413e2b2bd23e6d7a3059434d5
Si Thu Win <sthuwin@gmail.com>
2021-05-06 23:28:17 +0800

    added routes for Home, Login and Register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-const App = () => (
-  <div className="App">
-    <p>React App3</p>
-  </div>
-);
+import Home from "./pages/Home";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  );
+};
 
 export default App;
